1c6c272aeb8d9d971188880a3fd5e8c461764b2d
Yagantireedy123 <yagantireddy96@gmail.com>
2022-11-21 14:21:35 +0530

    second commit

--- com.Trobin/src/test/java/com/Trobin/Trobin/OpenBrowser.java
+++ com.Trobin/src/test/java/com/Trobin/Trobin/OpenBrowser.java
@@ -3,10 +3,13 @@ package com.Trobin.Trobin;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
 
 
 public class OpenBrowser {
-	public static void main(String[] args) {
+	
+	@Test
+	public void open() {
 		
 	
 		
